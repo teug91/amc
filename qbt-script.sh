2f02d0682ee54acd597f3939c1ab2dfff4942571
Terje Uglebakken <teug91@gmail.com>
2020-01-15 22:34:38 +0100

    add missing quotes

--- qbt-script.sh
+++ qbt-script.sh
@@ -19,4 +19,4 @@ ARG_NAME=$2
 ARG_LABEL=$3
 
 
-filebot -script $SCRIPT --output=$OUTPUT --action duplicate --conflict skip -non-strict --log-file amc.log --def unsorted=y clean=y extractFolder=$OUTPUT plex=$PLEX movieFormat=$MOVIE docFormat=$DOC seriesFormat=$SERIES miniFormat=$MINI ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
+filebot -script "$SCRIPT" --output="$OUTPUT" --action duplicate --conflict skip -non-strict --log-file amc.log --def unsorted=y clean=y extractFolder="$OUTPUT" plex="$PLEX" movieFormat="$MOVIE" docFormat="$DOC" seriesFormat="$SERIES" miniFormat="$MINI" ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
